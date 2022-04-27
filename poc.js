c48fcf1ecdcc54e1960943958fd9baff9198c164
Darren Place <48530717+darrenplace@users.noreply.github.com>
2022-04-27 14:04:23 -0500

    test on hardware, update documentation

--- poc.js
+++ poc.js
@@ -5,5 +5,10 @@ const dmx = new DMX();
 
 const universe = dmx.addUniverse('demo', config.driver, config.deviceId);
 
-//universe.update({channel: 0-512, value: 0-255});
-universe.update({1: 1, 2: 255});
+// universe.update({dmxChannel: value, dmxChannel: value, etc});
+
+// on
+universe.update({ 1: 255, 2: 255, 3: 255, 4: 255, 5: 255, 6: 255, 7: 255, 8: 255, 9: 255, 10: 255, 11: 255, 12: 255 });
+
+// off
+// universe.update({ 1: 0, 2: 255, 3: 0, 4: 255, 5: 0, 6: 255, 7: 0, 8: 255, 9: 0, 10: 255, 11: 0, 12: 255 });
